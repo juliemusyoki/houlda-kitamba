0ac6887e86c7f09ba5a68b2322c7366be2f5e669
Juliet <musyoki.juliet011@gmail.com>
2023-10-26 12:09:51 +0300

    DONE

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Marketing Manager", "Events and Communication Specialist" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+});
+
